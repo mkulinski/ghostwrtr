901ee1f037a61c2cd94ad4b268f71fb52d0c8793
Michael Kulinski <me@michaelkulinski.com>
2016-03-01 04:53:22 +0000

    updated gems and production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
